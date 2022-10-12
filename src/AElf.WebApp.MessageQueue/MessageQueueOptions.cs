84f31358d4a86e5bc61edc8c7afbc808f2ed7ec8
Wangsz0706 <109058157+Wangsz0706@users.noreply.github.com>
2022-10-12 16:58:03 +0800

    Modifying Configuration Parameters

--- src/AElf.WebApp.MessageQueue/MessageQueueOptions.cs
+++ src/AElf.WebApp.MessageQueue/MessageQueueOptions.cs
@@ -6,6 +6,6 @@ public class MessageQueueOptions
     public string ChainId { get; set; } = "AElf";
     public long StartPublishMessageHeight { get; set; } 
     public int Period { get; set; } = 1000;
-    public int BlockCountPerPeriod =100;
-    public int ParallelCount= 10;
+    public int BlockCountPerPeriod { get; set; } 
+    public int ParallelCount { get; set; } 
 }
